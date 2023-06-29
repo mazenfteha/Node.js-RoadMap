a2042173792abb802fa8879367865dbbdd9c01dc
mazenfteha <mazn34903@gmail.com>
2023-06-29 16:44:37 -0700

    request query

--- express-tutorial/params-query.js
+++ express-tutorial/params-query.js
@@ -17,7 +17,32 @@ app.get('/api/products', (req, res) => {
 app.get('/api/products/:productID',(req, res) => {
     const {productID} = req.params;
     const singleProduct = products.find((product)=> product.id === Number(productID))
+    if(!singleProduct){
+        res.status(404).send('Product does not exist')
+    }
     res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID',(req, res) => {
+    console.log(req.params)
+    res.send('hello world')
+})
+
+app.get('/api/v1/query',(req, res) => { //query?name=mazen&id=1
+    //console.log(req.query)
+    const {search, limit } = req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=> {
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    res.json(sortedProducts)
+    //res.send('hello world')
 })
 
 app.listen(5000, ()=>{
