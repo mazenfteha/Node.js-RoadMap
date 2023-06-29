8b27d946e2fefccb353ab52140b7b083d36ddac6
mazenfteha <mazn34903@gmail.com>
2023-06-29 16:12:38 -0700

    basics json and params query

---
+++ express-tutorial/params-query.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const {products} =require('./data')
+
+app.get('/', (req, res)=>{
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProduct = products.map((product) => {
+        const {id,name,image} = product
+        return {id, name, image}
+    })
+    res.json(newProduct)
+})
+
+app.get('/api/products/:productID',(req, res) => {
+    const {productID} = req.params;
+    const singleProduct = products.find((product)=> product.id === Number(productID))
+    res.json(singleProduct)
+})
+
+app.listen(5000, ()=>{
+    console.log('Server is listening on port 5000..')
+})
