c92f8509562fee7f468837ac3cd665a4e98b820b
mazenfteha <mazn34903@gmail.com>
2023-07-05 18:35:09 -0700

    POST method

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -8,13 +8,29 @@ app.use(express.static('./methods-public'))
 //parse from data
 app.use(express.urlencoded({extended:false }))
 
+//parse json
+app.use(express.json())
+
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data : people})
 })
 
+app.post('/api/people', (req,res) => {
+    const { name } = req.body
+    if (!name) {
+      return res
+        .status(400)
+        .json({ success: false, msg: 'please provide name value' })
+    }
+    res.status(201).send({ success: true, person: name })
+})
+
 app.post('/login', (req, res) => {
-    console.log(req.body)
-    res.send('POST')
+    const {name} = req.body
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('please provide credentials')
 })
 
 app.listen(5000, () => {
