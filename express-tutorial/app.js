02a3f480eb61aa1b748459da91958cd2ea8f0a25
mazenfteha <mazn34903@gmail.com>
2023-10-14 21:02:40 -0700

    add row body to handle request size limits

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,5 +1,24 @@
 const express = require('express')
+const contentType = require('content-type')
+const getRawBody = require('raw-body')
+const bodyParser = require('body-parser')
+
 const app = express()
+
+// First, use the bodyParser middleware to handle JSON requests
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    getRawBody(req, {
+        length: req.headers['content-length'],
+        limit: '1mb',
+        encoding: contentType.parse(req).parameters.charset
+    }, function (err, string) {
+        if (err) return next(err)
+        req.text = string
+        next()
+    })
+})
 
 const peopleRouter = require('./routes/people')
 const authRouter = require('./routes/auth')
