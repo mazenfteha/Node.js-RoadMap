2c8d42ca39772d6a7a5fea65facc543e1b6db887
mazenfteha <mazn34903@gmail.com>
2024-02-19 12:09:14 -0800

    Auth0

---
+++ Auth0/app.js
@@ -0,0 +1,33 @@
+const express = require("express"); 
+const { auth, requiresAuth } = require("express-openid-connect"); 
+const app = express(); 
+require("dotenv").config(); 
+
+app.use(
+    auth({
+        authRequired: false, 
+        auth0Logout: true,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET,
+        idpLogout: true,
+    })
+);
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+// The /profile route will show the user profile as JSON
+app.get("/profile", requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`); 
+});
+
+//go to localhost:3000/login to test it out
